61cfdd99be224196460ffb675b96a05c35f29a16
sudokufan <alexrichardgeorge@protonmail.com>
2021-05-14 16:30:25 +0100

    attempt at show/hiding reset instructions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,12 +3,24 @@ document.getElementById('backButton').onclick = function () {
   window.history.back()
 }
 
+
+// hide password helper box
+let helperBox = document.getElementById('hiddenPasswordHelperBox')
+document.getElementById('hiddenPasswordHelper').onclick = function () {
+  if (helperBox.style.display === 'none') {
+    helperBox.style.display === 'block'
+  } else {
+    helperBox.style.display === 'none'
+  }
+}
+
+
 // post request for email filled in by user
 document.querySelector('button').onclick = function () {
-    let userEmail = document.getElementsByClassName('emailInput').value
-    
-    fetch("http://localhost:3005/customer/account/resetPassword", {
-      method: "post",
+    let userEmail = document.getElementById('emailInput').value
+
+    fetch('http://localhost:3005/customer/account/resetPassword', {
+      method: 'POST',
       body: JSON.stringify({ email: userEmail }),
     })
     .then((response) => response.json())
